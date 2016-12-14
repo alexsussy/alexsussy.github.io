e6234d68cc1db129d05ab85987b5a6eb00bc79ea
Alex Sussman <asussman95@gmail.com>
2016-12-13 19:44:20 -0500

    Revert "Buttons"
    This reverts commit 0ff566c5b8a303a832b1d28ee9c0a9eb86d3412f.

--- js/index.js
+++ js/index.js
@@ -4,41 +4,15 @@ var nameElement = document.getElementById('name'),
     bioElement  = document.getElementById('bio'),
     contactElement  = document.getElementById('contact');
 
+    
+
 var nameANIM, portBUTTON, blogBUTTON, bioBUTTON, contactBUTTON;
 
 nameANIM = loadAnimation(nameElement, 'svg', true, true, name_data);
-portBUTTON = loadAnimation(portElement, 'svg', false, true, portfolio_data);
+portBUTTON = loadAnimation(portElement, 'svg', false, false, portfolio_data);
 blogBUTTON = loadAnimation(blogElement, 'svg', false, false, portfolio_data);
 bioBUTTON = loadAnimation(bioElement, 'svg', false, false, portfolio_data);
 contactBUTTON = loadAnimation(contactElement, 'svg', false, false, portfolio_data);
-
-portBUTTON.stop();
-
-portElement.onmouseenter = portAnimate_Enter;
-portElement.onmouseleave = portAnimate_Exit;
-
-function portAnimate_Enter(){
-    portBUTTON.setSpeed(1);
-    portBUTTON.setDirection(1);
-    portBUTTON.playSegments([[0,45],[46,300]],true);
-}
-
-function portAnimate_Exit(){
-    portBUTTON.setSpeed(2);
-    portBUTTON.setDirection(-1);
-    portBUTTON.playSegments([portBUTTON.currentFrame,0], true);
-    portBUTTON.addEventListener('loopComplete', portLoopComplete);
-}
-
-function portLoopComplete(){
-    portBUTTON.removeEventListener('loopComplete', portLoopComplete);
-    portBUTTON.stop();
-}
-
-
-
-
-
 
 
 function loadAnimation(container, renderer, autoplay, loop, data) {
