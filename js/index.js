9e67f7cfdea41b274aa7547d8ca6c924d144115a
Sussy <asussman95@gmail.com>
2016-12-14 16:17:22 -0500

    stuff

--- js/index.js
+++ js/index.js
@@ -9,31 +9,25 @@ var nameANIM, portBUTTON, blogBUTTON, bioBUTTON, contactBUTTON, gamesBUTTON;
 
 
 nameANIM = loadAnimation(nameElement, 'svg', true, false, name_data);
-portBUTTON = loadAnimation(portElement, 'svg', false, true, portfolio_data);
-blogBUTTON = loadAnimation(blogElement, 'svg', false, true, blog_data);
-bioBUTTON = loadAnimation(bioElement, 'svg', false, true, portfolio_data);
-contactBUTTON = loadAnimation(contactElement, 'svg', false, true, contact_data);
-gamesBUTTON = loadAnimation(gamesElement, 'svg', false, true, portfolio_data);
+portBUTTON = loadAnimation(portElement, 'svg', false, false, portfolio_data);
+blogBUTTON = loadAnimation(blogElement, 'svg', false, false, blog_data);
+bioBUTTON = loadAnimation(bioElement, 'svg', false, false, portfolio_data);
+contactBUTTON = loadAnimation(contactElement, 'svg', false, false, contact_data);
+gamesBUTTON = loadAnimation(gamesElement, 'svg', false, false, portfolio_data);
 
-
-portBUTTON.stop();
-blogBUTTON.stop();
-bioBUTTON.stop();
-contactBUTTON.stop();
 
 portElement.onmouseenter=
 function portAnimate_Enter(){
+    portBUTTON.play();
     portBUTTON.setSpeed(1);
     portBUTTON.setDirection(1);
-    portBUTTON.playSegments([[portBUTTON.currentFrame,45],[45,300]],true);
 }
 
 portElement.onmouseleave=
 function portAnimate_Exit(){
+    portBUTTON.play();
     portBUTTON.setDirection(-1);
     portBUTTON.setSpeed(2);
-    portBUTTON.playSegments([[portBUTTON.currentFrame,0],[0,0]],true);
-    portBUTTON.goToAndStop(0);
 }
 
 
@@ -41,15 +35,14 @@ blogElement.onmouseenter=
 function blogAnimate_Enter(){
     blogBUTTON.setSpeed(1);
     blogBUTTON.setDirection(1);
-    blogBUTTON.playSegments([[blogBUTTON.currentFrame,45],[45,300]],true);
+    blogBUTTON.play();
 }
 
 blogElement.onmouseleave=
 function blogAnimate_Exit(){
     blogBUTTON.setDirection(-1);
     blogBUTTON.setSpeed(2);
-    blogBUTTON.playSegments([[blogBUTTON.currentFrame,0],[0,0]],true);
-    blogBUTTON.goToAndStop(0);
+    blogBUTTON.play();
 }
 
 
@@ -57,15 +50,14 @@ contactElement.onmouseenter=
 function contactAnimate_Enter(){
     contactBUTTON.setSpeed(1);
     contactBUTTON.setDirection(1);
-    contactBUTTON.playSegments([[contactBUTTON.currentFrame,45],[45,300]],true);
+    contactBUTTON.play();
 }
 
 contactElement.onmouseleave=
 function contactAnimate_Exit(){
     contactBUTTON.setDirection(-1);
     contactBUTTON.setSpeed(2);
-    contactBUTTON.playSegments([[contactBUTTON.currentFrame,0],[0,0]],true);
-    contactBUTTON.goToAndStop(0);
+    contactBUTTON.play();
 }
 
 
